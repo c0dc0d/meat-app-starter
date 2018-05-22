d8f96c278e5dfbfb0f2494536d33964fabda4a24
igor <igor.soares.de.freitas@gmail.com>
2018-05-21 22:17:06 -0300

    Implementacao de restaurant-detail

--- src/app/restaurants-detail/restaurants-detail.component.ts
+++ src/app/restaurants-detail/restaurants-detail.component.ts
@@ -1,4 +1,8 @@
+import { Restaurant } from './../restaurants/restaurant/restaurant.model';
 import { Component, OnInit } from '@angular/core';
+
+import { RestaurantsService } from './../restaurants/restaurants.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'mt-restaurants-detail',
@@ -6,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantsDetailComponent implements OnInit {
 
-  constructor() { }
+  restaurant: Restaurant
+
+  constructor(private restaurantsService: RestaurantsService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.restaurantsService.restaurantById(this.route.snapshot.params['id'])
+      .subscribe(restaurant => this.restaurant = restaurant)
   }
 
 }
