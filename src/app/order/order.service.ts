d30675ce0276af95f45b206ffcfbcb91cde8a1c1
igor <igor.soares.de.freitas@gmail.com>
2018-06-18 20:04:38 -0300

    Implementacao de finalizacao de compra

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,12 +1,15 @@
+import { Http, Headers, RequestOptions } from '@angular/http';
 import { Injectable } from '@angular/core';
 
 import { ShoppingCartService } from '../restaurants-detail/shopping-cart/shopping-cart.service';
 import { CartItem } from '../restaurants-detail/shopping-cart/cart.item.model';
+import { Order } from './order.model';
+import { MEAT_API } from './../app.api';
 
 @Injectable()
 export class OrderService {
 
-  constructor(private cartService: ShoppingCartService) { }
+  constructor(private cartService: ShoppingCartService, private http: Http) { }
 
   cartItems(): CartItem[] {
     return this.cartService.items
@@ -26,5 +29,18 @@ export class OrderService {
 
   remove(item: CartItem) {
     this.cartService.removeItem(item)
+  }
+
+  clear() {
+    this.cartService.clear()
+  }
+
+  checkOrder(order: Order) {
+    const headers = new Headers()
+    headers.append('Content-Type', 'application/json')
+    return this.http.post(`${MEAT_API}/orders`,
+                          JSON.stringify(order),
+                          new RequestOptions({headers: headers}))
+                    .map(response => response.json())
   }
 }
